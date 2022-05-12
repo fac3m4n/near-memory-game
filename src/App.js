b001082f53ba3423a7eb059f5a910ac17e52934b
lanxeon <bswapnonil@yahoo.com>
2022-05-12 21:41:51 +0530

    added global state for account

--- src/App.js
+++ src/App.js
@@ -4,43 +4,89 @@ import { login, logout as destroy, accountBalance } from "./utils/near";
 import Wallet from "./components/wallet/wallet";
 // import { Notification } from "./components/utils/Notifications";
 // import Products from "./components/marketplace/Products";
-import Cover from "./components/home-page/home-page";
+import HomePage from "./components/home-page/home-page";
 import "./App.css";
+import useAccount from "./store/account";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = function AppWrapper() {
-  const account = window.walletConnection.account();
-  const [balance, setBalance] = useState("0");
-  const getBalance = useCallback(async () => {
-    if (account.accountId) {
-      setBalance(await accountBalance());
-    }
-  }, [account.accountId]);
+  const {
+    account,
+    accountId,
+    balance,
+    isWalletConnected,
+    setAccount,
+    setBalance,
+  } = useAccount();
 
   useEffect(() => {
-    getBalance();
-  }, [getBalance]);
+    const acc = window.walletConnection.account();
+    if (acc && acc.accountId) {
+      setAccount(acc);
+      accountBalance().then((bal) => setBalance(bal));
+    }
+  }, []);
+
+  // const [balance, setBalance] = useState("0");
+  // const getBalance = useCallback(async () => {
+  //   if (account.accountId) {
+  //     setBalance(await accountBalance());
+  //   }
+  // }, [account.accountId]);
+
+  // useEffect(() => {
+  //   getBalance();
+  // }, [getBalance]);
 
   return (
-    <>
-      {/* <Notification /> */}
-      {account.accountId ? (
-        <Container fluid="md">
-          <Nav className="justify-content-end pt-3 pb-5">
-            <Nav.Item>
+    // <>
+    //   {/* <Notification /> */}
+    //   {account.accountId ? (
+    //     <Container fluid="md">
+    //       <Nav className="justify-content-end pt-3 pb-5">
+    //         <Nav.Item>
+    //           <Wallet
+    //             address={account.accountId}
+    //             amount={balance}
+    //             symbol="NEAR"
+    //             destroy={destroy}
+    //           />
+    //         </Nav.Item>
+    //       </Nav>
+    //       <main>{/* <Products /> */}</main>
+    //     </Container>
+    //   ) : (
+    //     <Cover name="ZOO Memory" login={login} />
+    //   )}
+    // </>
+    <BrowserRouter>
+      {/* <Container fluid="md"> */}
+      {/* Navbar */}
+      {isWalletConnected && (
+        <Nav className="justify-content-end pt-3 pb-5">
+          <Nav.Item>
+            {isWalletConnected && (
               <Wallet
-                address={account.accountId}
+                address={account?.accountId}
                 amount={balance}
                 symbol="NEAR"
                 destroy={destroy}
               />
-            </Nav.Item>
-          </Nav>
-          <main>{/* <Products /> */}</main>
-        </Container>
-      ) : (
-        <Cover name="ZOO Memory" login={login} />
+            )}
+          </Nav.Item>
+        </Nav>
       )}
-    </>
+
+      <main>
+        <Routes>
+          <Route
+            path="/"
+            element={<HomePage name="ZOO Memory" login={login} />}
+          />
+        </Routes>
+      </main>
+      {/* </Container> */}
+    </BrowserRouter>
   );
 };
 
---
+++ src/store/account.js
@@ -0,0 +1,18 @@
+import create from "zustand";
+
+export const useAccount = create((set) => ({
+  account: null,
+  accountId: "",
+  isWalletConnected: false,
+  balance: "0",
+  balanceNumber: 0,
+
+  setAccount(account) {
+    set({ account, accountId: account.id, isWalletConnected: true });
+  },
+  setBalance(balance) {
+    set({ balance, balanceNumber: +balance });
+  },
+}));
+
+export default useAccount;

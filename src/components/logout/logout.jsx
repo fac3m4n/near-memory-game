2aa4b81519f79b8867822306383f65ae9252197e
lanxeon <bswapnonil@yahoo.com>
2022-05-15 14:19:48 +0530

    patched logout logic

--- src/components/logout/logout.jsx
+++ src/components/logout/logout.jsx
@@ -4,16 +4,17 @@ import { logout } from "../../utils/near";
 import useAccount from "../../store/account.store";
 
 const Logout = ({ color = "#888680", top = "5%", right = "5%" }) => {
-  const { isWalletConnected } = useAccount();
+  const { isWalletConnected, accountId } = useAccount();
+
+  if (!isWalletConnected || !accountId) return null;
 
   return (
     <div className={`d-flex flex-row ${classes.logout}`} style={{ top, right }}>
-      <span style={{ color }}>ZOO.NEAR</span>
-      {isWalletConnected && (
-        <button type="button" onClick={logout}>
-          {"(>"}
-        </button>
-      )}
+      <span style={{ color }}>{accountId}</span>
+
+      <button type="button" onClick={logout}>
+        {"(>"}
+      </button>
     </div>
   );
 };
